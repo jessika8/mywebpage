628d083e4cc68153d60bb71327bc9d3318f95524
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-03-19 11:17:25 +0000

    Updates

---
+++ precache-manifest.41b6c6e171fbcddac36060e8eb38d829.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6682750ed153ef4bae88cabad32dfa5",
+    "url": "/mywebpage/index.html"
+  },
+  {
+    "revision": "95f4772a16ca530a0d05",
+    "url": "/mywebpage/static/css/main.f08779f3.chunk.css"
+  },
+  {
+    "revision": "ece777d1efaa586d10a1",
+    "url": "/mywebpage/static/js/2.0c16aa35.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/mywebpage/static/js/2.0c16aa35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95f4772a16ca530a0d05",
+    "url": "/mywebpage/static/js/main.8f071636.chunk.js"
+  },
+  {
+    "revision": "7699d438be62dad100f7",
+    "url": "/mywebpage/static/js/runtime-main.e8d93438.js"
+  },
+  {
+    "revision": "d184cfe9b7de4558022a47d525e8ac9e",
+    "url": "/mywebpage/static/media/blogmy.d184cfe9.png"
+  },
+  {
+    "revision": "ddb7dbea07090bbd8b833dde3e56b232",
+    "url": "/mywebpage/static/media/movie.ddb7dbea.png"
+  },
+  {
+    "revision": "1f8460fd2c79641ef7588e1c0cb86847",
+    "url": "/mywebpage/static/media/todo.1f8460fd.png"
+  },
+  {
+    "revision": "492ea2b7e4797911af18b2462748ce8d",
+    "url": "/mywebpage/static/media/weather.492ea2b7.png"
+  }
+]);
